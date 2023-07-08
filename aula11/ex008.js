9499aab62bf7e55ccb4dccdbc4ffb50a96978d9c
HenricleyGuarnieri <henricleyg@gmail.com>
2023-07-08 20:35:14 -0300

    update

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 70
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+    if (vel > 60) { // Condição Simples!
+         console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+    }
+
+console.log(`Dirija sempre usando cinto de segurança`)
---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,51 @@
+function verificar () {
+   var data = new Date()
+   var ano = data.getFullYear()
+   var fano = document.getElementById('txtano')
+   var res = document.querySelector('div#res')
+   if (fano.value.length == 0 || Number(fano.value) > ano) {
+    window.alert('Verifique os dados e tente novamente!')
+   } else {
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fano.value)
+    var gênero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+
+    if (fsex[0].checked) {
+        gênero = 'Homem'
+        if (idade >= 0 && idade < 10) {
+         // Criança
+         img.setAttribute('src', 'bebemenino.png')
+        } else if (idade < 21) {
+         // Jovem
+         img.setAttribute('src', 'homemjovem.png')
+        } else if (idade < 55) {
+         // Adulto
+         img.setAttribute('src', 'homemadulto.png')
+        } else {
+         // Idoso
+         img.setAttribute('src', 'homemidoso.png')
+        }
+    } else if (fsex[1].checked) {
+        gênero = 'Mulher'
+        if (idade >= 0 && idade < 10) {
+            // Criança
+            img.setAttribute('src', 'bebemenina.png')
+           } else if (idade < 21) {
+            // Jovem
+            img.setAttribute('src', 'mulherjovem.png')
+           } else if (idade < 55) {
+            // Adulto
+            img.setAttribute('src', 'mulheradulta.png')
+           } else {
+            // Idoso
+            img.setAttribute('src', 'mulheridosa.png')
+           }
+    } 
+    res.style.textAlign = 'center'
+    res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+    res.appendChild(img)
+    
+   }
+}
